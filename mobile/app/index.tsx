505b90222bb413b53520a0651bfe9edc77ebce69
Prakash S <56394424+prakashcelt@users.noreply.github.com>
2025-11-22 22:51:56 +0530

    Fix mobile folder: remove submodule reference and add files

---
+++ mobile/app/index.tsx
@@ -0,0 +1,13 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import "../global.css";
+
+export default function Index() {
+  return (
+    <SafeAreaView className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to Nativewind!
+      </Text>
+    </SafeAreaView>
+  );
+}
